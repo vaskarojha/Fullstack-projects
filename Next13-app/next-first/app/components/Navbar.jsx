8010dd24b2368874fa98a43d2310a18b00d05a39
vaskarojha <vaskar.1125@gmail.com>
2024-01-18 11:42:13 +0100

    Navigation and styling

---
+++ Next13-app/next-first/app/components/Navbar.jsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import Logo from './logo.png'
+function Navbar() {
+  return (
+    <nav>
+        <Image 
+        src = {Logo}
+        alt = {'Website Logo'}
+        width = {70}
+        quality = {100}
+        placeholder = 'blur'
+        />
+    <Link href = '/'>Dashboard</Link>
+    <Link href='/tickets'>Tickets</Link>
+    </nav>
+  )
+}
+
+export default Navbar
