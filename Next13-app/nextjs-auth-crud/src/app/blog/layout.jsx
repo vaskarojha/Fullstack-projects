497e9a6f352159c25a015bafb3a2ac760f184b1b
vaskarojha <vaskar.1125@gmail.com>
2024-01-20 20:06:01 +0100

    layouts and components

---
+++ Next13-app/nextjs-auth-crud/src/app/blog/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function BlogLayout({children}) {
+  return (
+    <div>
+        <h1>from blog layout.</h1>
+        {children}
+        </div>
+  )
+}
+
+export default BlogLayout
---
+++ Next13-app/nextjs-auth-crud/src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <div>2024 all right reserved</div>
+        <div></div>
+    </div>
+  )
+}
