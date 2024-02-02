12c98889bda4985ef32a221780133ea047379ce1
vaskarojha <vaskar.1125@gmail.com>
2024-02-02 07:44:27 +0100

    add model

---
+++ Next13-app/auth-nextjs-app/src/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:[true,"Provide username"],
+        unique:true
+    },
+    email:{
+        type:String,
+        required:[true,"Provide email"],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,"Provide password"]
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+    forgetPasswordToken:String,
+    forgetPasswordTokenExpiry:Date,
+    verifyTokenExpiry: Date
+})
+
+const User = mongoose.models.users || mongoose.model("nextUser", userSchema);
+
+export default User;
