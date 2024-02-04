db7f3a9b02242aba7c96942fd26c17e33c14aced
vaskarojha <vaskar.1125@gmail.com>
2024-02-04 10:57:35 +0100

    added logout

---
+++ Next13-app/auth-nextjs-app/src/app/api/users/logout/route.ts
@@ -0,0 +1,17 @@
+import {NextResponse} from "next/server"
+
+export async function GET(){
+    try {
+        const response = NextResponse.json({message: "logout successfull.", success:true},
+        )
+        response.cookies.set("token", "", {
+            httpOnly:true,
+            expires: new Date(0)
+        })
+        return response
+    } catch (error:any) {
+        console.log("Error on logout: ", error.message)
+    }
+    return 
+
+}
--- Next13-app/auth-nextjs-app/src/app/profile/page.tsx
+++ Next13-app/auth-nextjs-app/src/app/profile/page.tsx
@@ -1,10 +1,24 @@
+"use client"
+import axios from "axios"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+
 export default function ProfilePage(){
+    const router = useRouter()
+    const logout=async ()=>{
+        try {
+            await axios.get('/api/users/logout')
+            router.push('/')
+        } catch (error: any) {
+            console.log("Error on logout out")
+        }
+    }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py2">
             <h1>Profile</h1>
             <hr />
             <p>User Profile Page</p>
-            
+            <button onClick={logout} className="p-2 bg-blue-300 rounded border-2 border-red-200">Logout</button>
         </div>
     )
 }
