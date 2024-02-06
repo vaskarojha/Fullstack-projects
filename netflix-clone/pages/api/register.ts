1c0d37cc8a06a4d7fea3be83419b9aeeab54c949
vaskarojha <vaskar.1125@gmail.com>
2024-02-06 09:36:35 +0100

    added register using next-auth

---
+++ netflix-clone/pages/api/register.ts
@@ -0,0 +1,34 @@
+import bcrypt from 'bcrypt'
+import { NextApiRequest, NextApiResponse } from 'next'
+import prismadb from '@/lib/prismadb'
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse){
+    if(req.method !=='POST'){
+        return res.status(405).end();
+    }
+    try {
+        const{email, name, password}= req.body;
+        const userExists = await prismadb.user.findUnique({
+            where:{
+                email
+            }
+        })
+        if(userExists){
+            return res.status(400).json({error: "Email already exists."})
+        }
+        const hashedPassword = await bcrypt.hash(password,12)
+        const user = await prismadb.user.create({
+            data:{
+                email,
+                name,
+                hashedPassword,
+                image:"",
+                emailVerified: new Date()
+            }
+        })
+        return res.json(user)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).end()
+    }
+}
--- netflix-clone/pages/auth.tsx
+++ netflix-clone/pages/auth.tsx
@@ -1,4 +1,5 @@
 import Input from "@/components/Input"
+import axios from "axios"
 import { useCallback, useState } from "react"
 const auth= ()=>{
     const [email, setEmail] = useState('')
@@ -9,6 +10,19 @@ const auth= ()=>{
     const toggleVariant = useCallback(()=>{
         setVariant((currentVariant)=>currentVariant==='login' ?'register':'login')
     }, [])
+
+    const register = useCallback(async()=>{
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            })
+        } catch (error) {
+            console.log(error)
+            
+        }
+    }, [email, name, password])
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -25,9 +39,9 @@ const auth= ()=>{
                             }
                             <Input label= "Email" onChange={(e)=>( setEmail(e.target.value))} id= "email" type= "email" value={email}/>
                          
-                            <Input label= "Password" onChange={(e)=>( setName(e.target.value))} id= "Password" type= "password" value={password}/>
+                            <Input label= "Password" onChange={(e)=>( setPassword(e.target.value))} id= "Password" type= "password" value={password}/>
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                         {variant === "login"? "Login":"Sign up"}
                         </button>
                         <p className="text-neutral-500 mt-12 ">
