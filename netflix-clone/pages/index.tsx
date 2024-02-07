5c47482dff3e0afd13e71381a2bfdabba77b0ff1
vaskarojha <vaskar.1125@gmail.com>
2024-02-07 08:32:44 +0100

    Added routes protection functionality

--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -1,8 +1,28 @@
+import { NextPageContext } from 'next'
+import {getSession, signOut} from 'next-auth/react'
+
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context);
+
+  if(!session){
+    return{
+      redirect:{
+        destination:'/auth',
+        permanent:false
+      }
+    }
+  }
+  return {
+    props:{}
+  }
+
+}
 
 export default function Home() {
   return (
     <>
     <h1 className="text-red-300">Hello</h1>
+    <button onClick={()=>signOut()} className='text-white bg-green-400 p-4'>Logout</button>
     </>
   )
 }
