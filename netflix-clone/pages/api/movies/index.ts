d6361047ac2808f266de069a80ca8fe83d2b8c3c
vaskarojha <vaskar.1125@gmail.com>
2024-02-11 11:10:37 +0100

    Added movielist hook and movie card component

--- netflix-clone/pages/api/movies/index.ts
+++ netflix-clone/pages/api/movies/index.ts
@@ -8,7 +8,7 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
     }
     try {
         await serverAuth(req)
-        const movies = prismadb.movie.findMany()
+        const movies = await prismadb.movie.findMany()
         return res.status(200).json(movies)
     } catch (error) {
         console.log(error)
